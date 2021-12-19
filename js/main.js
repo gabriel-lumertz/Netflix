79145f7900817d70d9d7ff5e360cd02dfea44797
Gabriel <glumertz10@gmail.com>
2021-12-19 04:26:01 -0300

    projeto finalizado

---
+++ js/main.js
@@ -0,0 +1,119 @@
+narcos_poster = "../img/narcos.jpg"
+narcos_video = "https://www.youtube.com/watch?v=GeTzNdlAEA0&t=3s"
+narcos_wiki = "https://pt.wikipedia.org/wiki/Narcos"
+
+function narcos(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${narcos_poster})`
+    document.getElementById("video").setAttribute("href", narcos_video)
+    document.getElementById("wiki").setAttribute("href", narcos_wiki)
+    document.getElementById("titulo").innerHTML = "Narcos"
+    document.getElementById("descricao").innerHTML = "A notória série sobre o tráfico é baseada na história real de violência e poder dos cartéis colombianos."
+}
+
+casa_poster = "../img/casa.jpg"
+casa_video = "https://www.youtube.com/watch?v=iS5xXr-GOnM"
+casa_wiki = "https://pt.wikipedia.org/wiki/La_casa_de_papel"
+
+function casa(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${casa_poster})`
+    document.getElementById("video").setAttribute("href", casa_video)
+    document.getElementById("wiki").setAttribute("href", casa_wiki)
+    document.getElementById("titulo").innerHTML = "La casa de papel"
+    document.getElementById("descricao").innerHTML = "Oito ladrões se trancam com reféns na Casa da Moeda da Espanha. Seu líder manipula a polícia para realizar um plano. Será o maior roubo da história, ou uma missão em vão?"
+}
+
+round_poster = "../img/round.jpg"
+round_video = "https://www.youtube.com/watch?v=Ncra_hUVtMM&t=19s"
+round_wiki = ""
+
+function round(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${round_poster})`
+    document.getElementById("video").setAttribute("href", round_video)
+    document.getElementById("wiki").setAttribute("href", round_wiki)
+    document.getElementById("titulo").innerHTML = "Round 6"
+    document.getElementById("descricao").innerHTML = "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Um prêmio milionário aguarda, mas as apostas são altas e mortais."
+}
+
+peaky_poster = "../img/peaky.jpg"
+peaky_video = "https://www.youtube.com/watch?v=Uv8bfaxl9j4"
+peaky_wiki = "https://pt.wikipedia.org/wiki/Peaky_Blinders_(s%C3%A9rie_de_televis%C3%A3o)"
+
+function peaky(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${peaky_poster})`
+    document.getElementById("video").setAttribute("href", peaky_video)
+    document.getElementById("wiki").setAttribute("href", peaky_wiki)
+    document.getElementById("titulo").innerHTML = "Peaky Blinders: Sangue, Apostas e Navalhas"
+    document.getElementById("descricao").innerHTML = "Uma notória gangue da Inglaterra de 1919 é liderada pelo cruel Tommy Shelby, um criminoso disposto a subir na vida a qualquer preço."
+}
+
+vikings_poster = "../img/vikings.jpg"
+vikings_video = "https://www.youtube.com/watch?v=Bt6gHmByX9U"
+vikings_wiki = "https://pt.wikipedia.org/wiki/Vikings_(s%C3%A9rie_de_televis%C3%A3o)"
+
+function vikings(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${vikings_poster})`
+    document.getElementById("video").setAttribute("href", vikings_video)
+    document.getElementById("wiki").setAttribute("href", vikings_wiki)
+    document.getElementById("titulo").innerHTML = "Vikings"
+    document.getElementById("descricao").innerHTML = "Esta série dramática acompanha a vida do viking Ragnar Lothbrok em sua jornada para ampliar o domínio nórdico e desafiar um líder incompetente e sem visão."
+}
+
+breaking_poster = "../img/breaking.jpeg"
+breaking_video = "https://www.youtube.com/watch?v=a0Vb8olwSwk"
+breaking_wiki = "https://pt.wikipedia.org/wiki/Breaking_Bad"
+
+function breaking(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${breaking_poster})`
+    document.getElementById("video").setAttribute("href", breaking_video)
+    document.getElementById("wiki").setAttribute("href", breaking_wiki)
+    document.getElementById("titulo").innerHTML = "Breaking Bad"
+    document.getElementById("descricao").innerHTML = "Ao saber que tem câncer, um professor passa a fabricar metanfetamina pelo futuro da família, mudando o destino de todos."
+}
+
+demon_poster = "../img/demon.jpg"
+demon_video = "https://www.youtube.com/watch?v=U3WXcj5yS48"
+demon_wiki = "https://pt.wikipedia.org/wiki/Kimetsu_no_Yaiba"
+
+function demon(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${demon_poster})`
+    document.getElementById("video").setAttribute("href", demon_video)
+    document.getElementById("wiki").setAttribute("href", demon_wiki)
+    document.getElementById("titulo").innerHTML = "Demon Slayer: Kimetsu no Yaiba"
+    document.getElementById("descricao").innerHTML = "Demônios massacraram sua família e amaldiçoaram sua irmã. Agora Tanjiro inicia sua jornada para encontrar a cura e se vingar."
+}
+
+demolidor_poster = "../img/demolidor.jpg"
+demolidor_video = "https://www.youtube.com/watch?v=y3GkQ6yoAA4"
+demolidor_wiki = "https://pt.wikipedia.org/wiki/Demolidor"
+
+function demolidor(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${demolidor_poster})`
+    document.getElementById("video").setAttribute("href", demolidor_video)
+    document.getElementById("wiki").setAttribute("href", demolidor_wiki)
+    document.getElementById("titulo").innerHTML = "Marvel - Demolidor"
+    document.getElementById("descricao").innerHTML = "Cego desde pequeno, Matt Murdock luta contra a injustiça durante o dia como advogado e à noite nas ruas de Hell’s Kitchen, Nova York."
+}
+
+dark_poster = "../img/dark.jpg"
+dark_video = "https://www.youtube.com/watch?v=JkUWmxhtU4E"
+dark_wiki = "https://pt.wikipedia.org/wiki/Dark"
+
+function dark(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${dark_poster})`
+    document.getElementById("video").setAttribute("href", dark_video)
+    document.getElementById("wiki").setAttribute("href", dark_wiki)
+    document.getElementById("titulo").innerHTML = "Dark"
+    document.getElementById("descricao").innerHTML = "Quatro famílias iniciam uma desesperada busca por respostas quando uma criança desaparece e um complexo mistério envolvendo três gerações começa a se revelar."
+}
+
+lupin_poster = "../img/lupin.jpg"
+lupin_video = "https://www.youtube.com/watch?v=FoiQ-Xr8NDI"
+lupin_wiki = "https://pt.wikipedia.org/wiki/Ars%C3%A8ne_Lupin"
+
+function lupin(){
+    document.getElementById("filme-principal").style.backgroundImage = `url(${lupin_poster})`
+    document.getElementById("video").setAttribute("href", lupin_video)
+    document.getElementById("wiki").setAttribute("href", lupin_wiki)
+    document.getElementById("titulo").innerHTML = "Lupin"
+    document.getElementById("descricao").innerHTML = "Inspirado pelas aventuras de Arsène Lupin, o ladrão gentil Assane Diop quer se vingar de uma família rica por uma injustiça cometida contra o pai dele."
+}
